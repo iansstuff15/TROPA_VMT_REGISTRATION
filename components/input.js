e961cfa58644b4c5b41f988dd9d44a1d9d234404
iansstuff15 <dlpowell35@gmail.com>
2022-03-27 01:16:00 +0800

    removed error causing attribute (for)

--- components/input.js
+++ components/input.js
@@ -2,7 +2,7 @@ import styles from '../styles/input.module.css'
 
 const InputComponent = ({name,label,type, ...otherProps})=>(
     <div>
-<label for={name} className={styles.label}>{label}</label>
+<label className={styles.label}>{label}</label>
   <input id={name} type={type} {...otherProps} className={styles.input}/>
     </div>
 )
