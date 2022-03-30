5f5ab74b14bac9fc3800481c15f205582354ef49
iansstuff15 <dlpowell35@gmail.com>
2022-03-31 04:21:27 +0800

    removed error causing link

--- components/data_privacy.js
+++ components/data_privacy.js
@@ -83,7 +83,7 @@ While using this site, we may ask you to provide us with certain personally iden
 <p>If you have any questions about this Privacy Policy, you can contact us:
 </p>
 <ul>
-    <li>By visiting this page on our website: <Link  href="https://www.facebook.com/solidlenipilipinas"><strong>https://www.facebook.com/solidlenipilipinas</strong></Link></li>
+    <li>By visiting this page on our website:<strong>https://www.facebook.com/solidlenipilipinas</strong></li>
 </ul>
     <input type={'checkbox'} required className={styles.checkbox} onClick={this.handleTick}/><span> I agree</span>
     </div>}
