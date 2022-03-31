c91d191c3100455136cd0962d70628eb8805a7af
iansstuff15 <dlpowell35@gmail.com>
2022-03-31 19:15:52 +0800

    added solid leni pilipinas logo

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 
+import logo from '../public/logo.png'
 import styles from '../styles/Home.module.css'
 import Background from '../public/background_image.jpg'
 export default function Home() {
   return (
     <div className={styles.home}>    <div className={styles.container}>
-      
+         <Image src={logo}alt="logo"
+      width={100}
+      height={100}/>
       <div className={styles.container_vertical_center}>
       <h3 className={styles.title} >Take part in the campaign</h3>
       <h1 className={styles.title_emphasis}>Be a TROPA <span className={styles.volunteer_text}>Volunteer</span></h1>
--- pages/register.js
+++ pages/register.js
@@ -1,8 +1,13 @@
 import styles from '../styles/register.module.css'
+import Image from 'next/image'
+import logo from '../public/logo.png'
 import FormComponent from '../components/form'
 const Register = ()=> (
 
     <div className={styles.container} >
+    <Image src={logo}alt="logo"
+      width={100}
+      height={100}/>
     <div className={styles.disclaimer}>
     <h3>Disclaimer</h3>
     <p> I am providing this information willingly, with the understanding that it will be used solely for the purpose of volunteering under Solid Leni Pilipinas and its activities in support for the presidential and vice presidential campaign of Leni Robredo and Kiko Pangilinan for Halalan 2022.</p>
